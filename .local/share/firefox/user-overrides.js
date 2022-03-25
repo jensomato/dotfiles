3e4f02babf6c7b6d83533aaf761969e006eddb8e
Jens Dürr <jens@jensomato.de>
2022-03-25 12:47:51 +0100

    add firefox config

---
+++ .local/share/firefox/user-overrides.js
@@ -0,0 +1,5 @@
+user_pref("browser.startup.page", 3); // 0102
+user_pref("browser.sessionstore.privacy_level", 0); // 1003
+user_pref("privacy.clearOnShutdown.history", false); // 2811
+user_pref("privacy.cpd.history", true); // 2812
+user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504
