190814ad9155bf30b62f9e9e46d55eb31ae63ef7
Jens Dürr <jens@jensomato.de>
2023-07-12 00:15:37 +0200

    fix issue with bookmark function

--- .config/zsh/zsh.d/functions.zsh
+++ .config/zsh/zsh.d/functions.zsh
@@ -1,6 +1,6 @@
 b() {
 	[[ ! -d $BOOKMARKS_DIR ]] && echo "BOOKMARKS_DIR not set" && return 1
-	result=$(find $BOOKMARKS_DIR/ -type l -printf "%P -> %l\\0" | fzf --read0)
+	result=$(find $BOOKMARKS_DIR/ -type l -printf "%P -> " -exec realpath -z {} \; | fzf --read0)
 	if [ "$?" -eq 0 ]; then
 		cd "${result/* -> /}"
 	fi
