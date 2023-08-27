869bf957462d9f7353396acfc5d0b3da8916666d
Jens Dürr <jens@jensomato.de>
2023-08-27 14:38:22 +0200

    update functions.zsh - add preview to b

--- .config/zsh/zsh.d/functions.zsh
+++ .config/zsh/zsh.d/functions.zsh
@@ -1,6 +1,6 @@
 b() {
 	[[ ! -d $BOOKMARKS_DIR ]] && echo "BOOKMARKS_DIR not set" && return 1
-	result=$(find $BOOKMARKS_DIR/ -type l -printf "%P -> " -exec realpath -z {} \; | fzf --read0)
+	result=$(find $BOOKMARKS_DIR/ -type l -printf "%P -> " -exec realpath -z {} \; | fzf -1 -q "$1" -e --read0 --preview='var="{}";var="${var%'\''}"; var="${var#'\''}"; var="${var/* -> /}"; ls -l --group-directories-first "$var"')
 	if [ "$?" -eq 0 ]; then
 		cd "${result/* -> /}"
 	fi
