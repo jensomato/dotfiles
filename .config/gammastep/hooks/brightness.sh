fe06cb809c499f0e17243fe393177f2bfc6f90b6
Jens Dürr <jens@jensomato.de>
2024-01-21 21:54:14 +0100

    update brightnes.sh gammastep hook

--- .config/gammastep/hooks/brightness.sh
+++ .config/gammastep/hooks/brightness.sh
@@ -1,9 +1,15 @@
-#!env bash
+#!/usr/bin/env bash
 brightness_day=65
 brightness_transition=45
 brightness_night=30
+color_day=06
+color_transition=06
+color_night=03
 set_brightness() {
 	ddcutil setvcp 10 $1 --display 1
+}
+set_color() {
+	ddcutil setvcp 14 $1 --display 1
 }
 if [ "$1" = period-changed ]; then
 	notify-send "period changed : \"$2\" to \"$3\""
@@ -15,6 +21,9 @@ if [ "$1" = period-changed ]; then
 			set_brightness $brightness_transition
 			;;
 		daytime)
+			set_brightness $brightness_day
+			;;
+		none)
 			set_brightness $brightness_day
 			;;
 	esac
