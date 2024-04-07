8185bc2d00f9eb114dd792c92d7be61427c1618b
Jens Dürr <jens@jensomato.de>
2024-04-07 22:45:16 +0200

    update gammastep brightness hook

--- .config/gammastep/hooks/brightness.sh
+++ .config/gammastep/hooks/brightness.sh
@@ -1,30 +1,64 @@
 #!/usr/bin/env bash
-brightness_day=65
-brightness_transition=45
-brightness_night=30
-color_day=06
-color_transition=06
-color_night=03
-set_brightness() {
-	ddcutil setvcp 10 $1 --display 1
+set_variables() {
+	brightness_apollo_day=65
+	brightness_apollo_transition=45
+	brightness_apollo_night=30
+	brightness_helo_day=140
+	brightness_helo_transition=120
+	brightness_helo_night=100
+	color_day=06
+	color_transition=06
+	color_night=03
+	hostname=$(hostname)
 }
+
+set_brightness_apollo() {
+	ddcutil setvcp 10 "$1" --display 1
+}
+
+set_brightness_helo() {
+	echo "$1" > /sys/class/backlight/amdgpu_bl0/brightness
+}
+
 set_color() {
-	ddcutil setvcp 14 $1 --display 1
+	ddcutil setvcp 14 "$1" --display 1
 }
-if [ "$1" = period-changed ]; then
-	notify-send "period changed : \"$2\" to \"$3\""
-	case $3 in
-		night)
-			set_brightness $brightness_night
-			;;
-		transition)
-			set_brightness $brightness_transition
-			;;
-		daytime)
-			set_brightness $brightness_day
-			;;
-		none)
-			set_brightness $brightness_day
-			;;
-	esac
-fi
+
+main() {
+	set_variables
+	if [ "$1" = period-changed ]; then
+		notify-send "period changed : \"$2\" to \"$3\""
+		case $3 in
+			night)
+				if [[ "$hostname" == "helo" ]]; then
+					set_brightness_helo $brightness_helo_night
+				else
+					set_brightness_apollo $brightness_apollo_night
+				fi
+				;;
+			transition)
+				if [[ "$hostname" == "helo" ]]; then
+					set_brightness_helo $brightness_helo_transition
+				else
+					set_brightness_apollo $brightness_apollo_transition
+				fi
+				;;
+			daytime)
+				if [[ "$hostname" == "helo" ]]; then
+					set_brightness_helo $brightness_helo_day
+				else
+					set_brightness_apollo $brightness_apollo_day
+				fi
+				;;
+			none)
+				if [[ "$hostname" == "helo" ]]; then
+					set_brightness_helo $brightness_helo_day
+				else
+					set_brightness_apollo $brightness_apollo_day
+				fi
+				;;
+		esac
+	fi
+}
+
+main "$@"
